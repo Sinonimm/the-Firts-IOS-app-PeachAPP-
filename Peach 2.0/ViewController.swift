a5189a7330d926966d4c2b2a6b83cdd2c69b5834
Sinonimm <shchemelev.ilia@gmail.com>
2022-02-05 11:24:50 +0300

    button and text change

--- Peach 2.0/ViewController.swift
+++ Peach 2.0/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btmNameChange: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        btmNameChange.layer.cornerRadius = 17.5
+    }
 
-
+    @IBAction func btnChageNamePress(_ sender: UIButton) {
+        lblTitle.text = "BARABARA"
+    }
 }
 
